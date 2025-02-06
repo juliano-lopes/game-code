9021f1fc3526efdfdd2b9883574dbe86bb896ec3
juliano-lopes <julopys@hotmail.com>
2025-02-06 15:34:45 -0300

    Adds guard to protect routs

---
+++ src/app/guards/auth.guard.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Observable } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.authService.user$.pipe(
+      take(1),
+      map(user => !!user), // <-- map to boolean
+      tap(loggedIn => {
+        if (!loggedIn) {
+          console.log("access denied")
+          this.router.navigate(['/login']);
+        }
+      })
+    )
+  }
+}
