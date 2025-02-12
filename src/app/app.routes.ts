bc6d4924c5c12bb92fea030abe88c0a101effbd7
juliano-lopes <julopys@hotmail.com>
2025-02-12 00:45:44 -0300

    Sets new routs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginComponent } from './components/login/login.component';
 import { ModuleListComponent } from './components/module-list/module-list.component';
 import { AuthGuard } from './guards/auth.guard.service';
 import { ExerciseListComponent } from './components/exercise-list/exercise-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 //import { RegisterComponent } from './modules/auth/register/register.component';
 //import { ModuleListComponent } from './modules/modules/module-list/module-list.component';
@@ -11,14 +13,15 @@ import { ExerciseListComponent } from './components/exercise-list/exercise-list.
 //import { AuthGuard } from './guards/auth.guard'; // Import your AuthGuard
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  //{ path: 'register', component: RegisterComponent },
-  { path: 'modules', component: ModuleListComponent, canActivate: [AuthGuard],
+  {
+    path: 'home', component: HomeComponent, canActivate: [AuthGuard],
     children: [
-      { path: ':moduleId/exercises', component: ExerciseListComponent, canActivate: [AuthGuard] }
+      { path: '', component: WelcomeComponent, canActivate: [AuthGuard] },
+      { path: 'exercises/:moduleId', component: ExerciseListComponent, canActivate: [AuthGuard] }
     ]
-   },
-  //{ path: 'exercises/:moduleId', component: ExerciseListComponent, canActivate: [AuthGuard] }
-  //{ path: 'exercise-execution/:exerciseId', component: ExerciseExecutionComponent, canActivate: [AuthGuard] }
+
+  }
+
 ];
