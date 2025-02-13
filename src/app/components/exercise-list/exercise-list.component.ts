b3809db616ed716aabd0d981f71e1150e4230691
juliano-lopes <julopys@hotmail.com>
2025-02-13 18:19:24 -0300

    Changes exercise--list to use button material e icon from material

--- src/app/components/exercise-list/exercise-list.component.ts
+++ src/app/components/exercise-list/exercise-list.component.ts
@@ -6,9 +6,11 @@ import { DataService } from '../../services/data.service';
 import { ActivatedRoute } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { ExerciseExecutionComponent } from '../exercise-execution/exercise-execution.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-exercise-list',
-  imports: [CommonModule, ExerciseExecutionComponent],
+  imports: [CommonModule, ExerciseExecutionComponent, MatButtonModule, MatIconModule],
   templateUrl: './exercise-list.component.html',
   styleUrl: './exercise-list.component.css'
 })
