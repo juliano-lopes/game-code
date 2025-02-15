3e0db0728846fc3c38c7b2fb5fbd8a325cdca816
juliano-lopes <julopys@hotmail.com>
2025-02-14 21:19:37 -0300

    Adds google functions and storage.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,16 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-//import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
+//import corsLib from 'cors';
 //import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//const cors = corsLib({ origin: 'https://staging-game-code-app.web.app' });
+export const helloWorld3 = onRequest({ cors: true }, (request, response) => {
+  //   logger.info("Hello logs!", {structuredData: true});
+  //cors(request, response, () => { // Wrap your function logic with cors()
+  response.status(200).send({ message: 'Olá, Mundo3!' });
+  //});
+});
