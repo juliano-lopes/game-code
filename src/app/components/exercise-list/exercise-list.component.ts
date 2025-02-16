6de95b831d663db5f51409b7cf3b041c9e0baaa2
juliano-lopes <julopys@hotmail.com>
2025-02-16 06:45:06 -0300

    Adds functionality to get the user completed exercises and show if an exercise hhas already been completed.

--- src/app/components/exercise-list/exercise-list.component.ts
+++ src/app/components/exercise-list/exercise-list.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, input, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { Exercise } from '../../types/exercise';
@@ -8,6 +8,8 @@ import { RouterLink } from '@angular/router';
 import { ExerciseExecutionComponent } from '../exercise-execution/exercise-execution.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { ResolutionService } from '../../services/resolution.service';
+import { Resolution } from '../../types/resolution';
 @Component({
   selector: 'app-exercise-list',
   imports: [CommonModule, ExerciseExecutionComponent, MatButtonModule, MatIconModule],
@@ -19,12 +21,13 @@ export class ExerciseListComponent implements OnInit, AfterViewInit {
   exercises$: Observable<Exercise[]> | undefined;
   exercises: Exercise[] = [];
   @Input() moduleName!: string;
-  @Input() moduleId: string | null = null;
+  @Input() moduleId: string | undefined = undefined;
   selectedExercise: Exercise | undefined;
+  resolutions: string[] = [];
+  constructor(private dataService: DataService, private route: ActivatedRoute, protected resolutionService: ResolutionService) { }
 
-  constructor(private dataService: DataService, private route: ActivatedRoute) { }
-
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    this.resolutions = await this.resolutionService.getExercisesCompleted();
     //this.moduleId = this.route.snapshot.paramMap.get('moduleId');
     //this.route.paramMap.subscribe(params => {
     //this.moduleId = params.get('moduleId');
@@ -32,13 +35,16 @@ export class ExerciseListComponent implements OnInit, AfterViewInit {
       this.exercises$ = this.dataService.createDataObject<Exercise>("exercises").getByField('moduleId', this.moduleId);
       this.exercises$.subscribe((exercises) => {
         this.exercises = exercises.sort((a, b) => a.number - b.number);
+
       });
+
     }
     //});
   }
   ngAfterViewInit(): void {
     this.title = `Exercícios ${this.moduleName}`;
     document.title = `${this.title} - Game Code App`;
+
   }
 
   onSelectExercise(exercise: Exercise) {
@@ -48,7 +54,7 @@ export class ExerciseListComponent implements OnInit, AfterViewInit {
     let id = this.selectedExercise ? this.selectedExercise.id : '';
     this.selectedExercise = undefined;
     setTimeout(() => {
-      let el =id ?  document.getElementById(id) : null;
+      let el = id ? document.getElementById(id) : null;
       el ? el.focus() : false;
     }, 500);
   }
--- src/app/services/resolution.service.ts
+++ src/app/services/resolution.service.ts
@@ -3,13 +3,17 @@ import { AuthService } from './auth.service';
 import { DataService } from './data.service';
 import { Resolution } from '../types/resolution';
 import { Timestamp } from '@firebase/firestore';
+import { FirebaseService } from './firebase.service';
+import { Exercise } from '../types/exercise';
+import { firstValueFrom, lastValueFrom, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResolutionService {
+  constructor(private auth: AuthService, private data: DataService) {
+  }
 
-  constructor(private auth: AuthService, private data: DataService) { }
   registerExerciseCompletion(exerciseId: string | undefined): Promise<boolean> {
     if (!exerciseId) {
       throw new Error("É preciso haver um id válido para registrar a finalização do exercício");
@@ -29,5 +33,13 @@ export class ResolutionService {
     } catch (error) {
       throw error;
     }
+  }
+  async getExercisesCompleted() {
+
+    const user = await firstValueFrom(this.auth.user$);
+    const resolutions = await firstValueFrom(this.data.createDataObject<Resolution>("resolution").getByField("userId", user.uid));
+    const resolutionsId = resolutions.map((resolution) => resolution.exerciseId);
+    return resolutionsId;
+
   }
 }
