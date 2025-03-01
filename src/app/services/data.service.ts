d015460ff01eb130ea7ec2caffe57b6f054947db
juliano-lopes <julopys@hotmail.com>
2025-03-01 18:04:57 -0300

    Removes unused code.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -16,42 +16,4 @@ export class DataService {
   createDataObject<T extends FirebaseDocument>(collectionName: string) {
     return new FirebaseService<T>(this.firestore, collectionName);
   }
-  getModules(): Observable<Module[]> {
-    const modulesCollection = collection(this.firestore, 'modules');
-    const modulesQuery = query(modulesCollection, orderBy("number"));
-    return collectionData(modulesQuery, { idField: 'id' }) as Observable<Module[]>;
-  }
-
-  getExercisesByModuleId(moduleId: string): Observable<Exercise[]> {
-    const exercisesCollection = collection(this.firestore, 'exercises');
-    const q = query(exercisesCollection, where('moduleId', '==', moduleId));
-    return collectionData(q, { idField: 'id' }) as Observable<Exercise[]>;
-  }
-
-  addExerciseResolution(userId: string, moduleId: string, exerciseId: string): Promise<any> {
-    const exerciseResolutionsCollection = collection(this.firestore, 'exercise_resolutions');
-    return addDoc(exerciseResolutionsCollection, {
-      userId: userId,
-      moduleId: moduleId,
-      exerciseId: exerciseId,
-      timestamp: new Date()
-    });
-  }
-
-  addExercise(exercise: Exercise): Promise<any> {
-    const exercisesCollection = collection(this.firestore, 'exercises');
-    return addDoc(exercisesCollection, exercise);
-  }
-
-  // Example: Update an existing document
-  updateModule(moduleId: string, data: any): Promise<void> {
-    const moduleDocRef = doc(this.firestore, `modules/${moduleId}`);
-    return setDoc(moduleDocRef, data, { merge: true });  // Use merge to update only specified fields
-  }
-
-  // Example: Delete a document
-  deleteModule(moduleId: string): Promise<void> {
-    const moduleDocRef = doc(this.firestore, `modules/${moduleId}`);
-    return deleteDoc(moduleDocRef);
-  }
 }
