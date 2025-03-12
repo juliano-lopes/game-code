f226b2cce3d955999bd5fe7c4184f25976b20963
juliano-lopes <julopys@hotmail.com>
2025-03-11 22:06:46 -0300

    Adds deploy to staging environment.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,6 +8,7 @@ import { MatInput } from '@angular/material/input';
 import { MatLabel } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-login',
   imports: [FormsModule, MatCardModule, MatFormField, MatInput, MatLabel, MatButtonModule, MatIconModule],
@@ -28,6 +29,13 @@ export class LoginComponent implements OnInit {
         this.router.navigate(['/home']);
       }
     });
+    if (environment.mode == "production") {
+      console.log("Em prod");
+    } else if (environment.mode == "staging") {
+      console.log("Em staging");
+    } else {
+      console.log("Em dev");
+    }
   }
 
   async onSubmit(): Promise<void> {
