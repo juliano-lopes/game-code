ae3e03f1fbc25c1e2f80f728e3f1b6bbde8a7076
juliano-lopes <julopys@hotmail.com>
2025-03-16 19:25:23 -0300

    Creates dataService spec with basic test checking if at least the service is created.

--- src/app/services/data.service.spec.ts
+++ src/app/services/data.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
+import { Firestore } from '@angular/fire/firestore';
 
 describe('DataService', () => {
   let service: DataService;
-
+  let mockFirestore: jasmine.SpyObj<Firestore>;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockFirestore = jasmine.createSpyObj('Firestore', ['collection']);
+    TestBed.configureTestingModule({
+      providers: [
+        DataService,
+        { provide: Firestore, useValue: mockFirestore }
+      ]
+    });
     service = TestBed.inject(DataService);
   });
 
