e57e47ec9d32967bdcdf98586db9961243534694
juliano-lopes <julopys@hotmail.com>
2025-03-16 19:15:51 -0300

    Creates a authservice test with the basic checking if the service is at least created.

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,12 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
+import { doc, setDoc, Firestore } from '@angular/fire/firestore';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let mockRouter: jasmine.SpyObj<Router>;
+  let mockAuth: jasmine.SpyObj<Auth>;
+  let mockFirestore: jasmine.SpyObj<Firestore>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockRouter = jasmine.createSpyObj('Router', ['navigate']);
+    mockAuth = jasmine.createSpyObj('Auth', ['createUserWithEmailAndPassword', 'signInWithEmailAndPassword', 'signOut']);
+    mockFirestore = jasmine.createSpyObj('Firestore', ['collection']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        AuthService,
+        { provide: Router, useValue: mockRouter },
+        { provide: Auth, useValue: mockAuth },
+        { provide: Firestore, useValue: mockFirestore },
+      ],
+    });
     service = TestBed.inject(AuthService);
   });
 
